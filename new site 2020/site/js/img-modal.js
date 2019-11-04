24e3b514163734fb37fbf9230e609e8361f3dcc0
Matt Grippo <grippom@amazon.com>
2019-11-04 13:09:47 -0500

    Moving site onto git

---
+++ new site 2020/site/js/img-modal.js
@@ -0,0 +1,118 @@
+//tab stuff
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+//second gallery on page
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function plusSlides2(n) {
+  showSlides2((slideIndex2 += n));
+}
+
+function currentSlide2(n) {
+  showSlides2((slideIndex2 = n));
+}
+
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("dot2");
+  if (n > slides.length) {
+    slideIndex2 = 1;
+  }
+  if (n < 1) {
+    slideIndex2 = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex2 - 1].style.display = "block";
+  dots[slideIndex2 - 1].className += " active";
+}
+
+//image modals
+
+var modal6 = document.getElementById("myModal6");
+var modal7 = document.getElementById("myModal7");
+
+var img6 = document.getElementById("myImg6");
+var modalImg6 = document.getElementById("img06");
+var captionText6 = document.getElementById("caption6");
+
+var img7 = document.getElementById("myImg7");
+var modalImg7 = document.getElementById("img07");
+var captionText7 = document.getElementById("caption7");
+
+img6.onclick = function() {
+  modal6.style.display = "block";
+  modalImg6.src = this.src;
+  captionText6.innerHTML = this.alt;
+};
+img7.onclick = function() {
+  modal7.style.display = "block";
+  modalImg7.src = this.src;
+  captionText7.innerHTML = this.alt;
+};
+
+modal6.onclick = function() {
+  modal6.style.display = "none";
+};
+
+modal7.onclick = function() {
+  modal7.style.display = "none";
+};
---
+++ new site 2020/site/js/main.js
@@ -0,0 +1,180 @@
+//other stuff
+
+(function() {
+  "use strict";
+
+  // iPad and iPod detection
+  var isiPad = function() {
+    return navigator.platform.indexOf("iPad") != -1;
+  };
+
+  var isiPhone = function() {
+    return (
+      navigator.platform.indexOf("iPhone") != -1 ||
+      navigator.platform.indexOf("iPod") != -1
+    );
+  };
+
+  // OffCanvass
+  var offCanvass = function() {
+    $("body").on(
+      "click",
+      ".js-fh5co-menu-btn, .js-fh5co-offcanvass-close",
+      function() {
+        $("#fh5co-offcanvass").toggleClass("fh5co-awake");
+      }
+    );
+  };
+
+  // Click outside of offcanvass
+  var mobileMenuOutsideClick = function() {
+    $(document).click(function(e) {
+      var container = $("#fh5co-offcanvass, .js-fh5co-menu-btn");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($("#fh5co-offcanvass").hasClass("fh5co-awake")) {
+          $("#fh5co-offcanvass").removeClass("fh5co-awake");
+        }
+      }
+    });
+
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 500) {
+        if ($("#fh5co-offcanvass").hasClass("fh5co-awake")) {
+          $("#fh5co-offcanvass").removeClass("fh5co-awake");
+        }
+      }
+    });
+  };
+
+  // Magnific Popup
+
+  var magnifPopup = function() {
+    $(".image-popup").magnificPopup({
+      type: "image",
+      removalDelay: 300,
+      mainClass: "mfp-with-zoom",
+      titleSrc: "title",
+      gallery: {
+        enabled: true
+      },
+      zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it
+
+        duration: 300, // duration of the effect, in milliseconds
+        easing: "ease-in-out", // CSS transition easing function
+
+        // The "opener" function should return the element from which popup will be zoomed in
+        // and to which popup will be scaled down
+        // By defailt it looks for an image tag:
+        opener: function(openerElement) {
+          // openerElement is the element on which popup was initialized, in this case its <a> tag
+          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+          return openerElement.is("img")
+            ? openerElement
+            : openerElement.find("img");
+        }
+      }
+    });
+  };
+
+  var animateBoxWayPoint = function() {
+    if ($(".animate-box").length > 0) {
+      $(".animate-box").waypoint(
+        function(direction) {
+          if (direction === "down" && !$(this).hasClass("animated")) {
+            $(this.element).addClass("bounceIn animated");
+          }
+        },
+        { offset: "75%" }
+      );
+    }
+  };
+
+  $(function() {
+    magnifPopup();
+    offCanvass();
+    mobileMenuOutsideClick();
+    animateBoxWayPoint();
+  });
+})();
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+var modal4 = document.getElementById("myModal4");
+var modal5 = document.getElementById("myModal5");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+var img2 = document.getElementById("myImg2");
+var modalImg2 = document.getElementById("img02");
+var captionText2 = document.getElementById("caption2");
+
+var img3 = document.getElementById("myImg3");
+var modalImg3 = document.getElementById("img03");
+var captionText3 = document.getElementById("caption3");
+
+var img4 = document.getElementById("myImg4");
+var modalImg4 = document.getElementById("img04");
+var captionText4 = document.getElementById("caption4");
+
+var img5 = document.getElementById("myImg5");
+var modalImg5 = document.getElementById("img05");
+var captionText5 = document.getElementById("caption5");
+
+img.onclick = function() {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+img2.onclick = function() {
+  modal2.style.display = "block";
+  modalImg2.src = this.src;
+  captionText2.innerHTML = this.alt;
+};
+img3.onclick = function() {
+  modal3.style.display = "block";
+  modalImg3.src = this.src;
+  captionText3.innerHTML = this.alt;
+};
+img4.onclick = function() {
+  modal4.style.display = "block";
+  modalImg4.src = this.src;
+  captionText4.innerHTML = this.alt;
+};
+img5.onclick = function() {
+  modal5.style.display = "block";
+  modalImg5.src = this.src;
+  captionText5.innerHTML = this.alt;
+};
+
+$(document).keyup(function(e) {
+  if (e.key === "Escape") {
+    // escape key maps to keycode `27`
+    modal.style.display = "none";
+  }
+});
+
+modal.onclick = function() {
+  modal.style.display = "none";
+};
+
+modal2.onclick = function() {
+  modal2.style.display = "none";
+};
+
+modal3.onclick = function() {
+  modal3.style.display = "none";
+};
+
+modal4.onclick = function() {
+  modal4.style.display = "none";
+};
+
+modal5.onclick = function() {
+  modal5.style.display = "none";
+};
