549562c82c535c3ad926855a1a55e176f258906d
Matt Grippo <matt@janz.io>
2019-11-28 14:14:35 -0500

    Updates from thanksgiving

--- new site 2020/site/js/img-modal.js
+++ new site 2020/site/js/img-modal.js
@@ -85,6 +85,39 @@ function showSlides2(n) {
   dots[slideIndex2 - 1].className += " active";
 }
 
+//third gallery
+
+var slideIndex3 = 1;
+showSlides3(slideIndex3);
+
+function plusSlides3(n) {
+  showSlides3((slideIndex3 += n));
+}
+
+function currentSlide3(n) {
+  showSlides3((slideIndex3 = n));
+}
+
+function showSlides3(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides3");
+  var dots = document.getElementsByClassName("dot3");
+  if (n > slides.length) {
+    slideIndex3 = 1;
+  }
+  if (n < 1) {
+    slideIndex3 = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex3 - 1].style.display = "block";
+  dots[slideIndex3 - 1].className += " active";
+}
+
 //image modals
 
 var modal6 = document.getElementById("myModal6");
