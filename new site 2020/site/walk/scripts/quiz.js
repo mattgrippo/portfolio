1e80ee129dcd446a5c950df3b2fd7a75be018d0e
Matt Grippo <grippom@amazon.com>
2019-12-02 16:22:08 -0500

    Changes from work on walk and about

--- new site 2020/site/walk/scripts/quiz.js
+++ new site 2020/site/walk/scripts/quiz.js
@@ -2,17 +2,24 @@
 // Question Script
 
 function correct(link) {
-	document.getElementById('quiz_return').innerHTML = "Correct Answer!";
+  document.getElementById("quiz_return").classList.remove("wrong");
+  document.getElementById("quiz_return").classList.add("correct");
+  document.getElementById("quiz_return").innerHTML = "Correct Answer!";
 
-	showLink(link);
+  showLink(link);
 }
 
 function wrong(link) {
-	document.getElementById('quiz_return').innerHTML = "Wrong Answer... Try again!";
-	showLink(link);
+  document.getElementById("quiz_return").classList.remove("correct");
+  document.getElementById("quiz_return").classList.add("wrong");
+  document.getElementById("quiz_return").innerHTML =
+    "Wrong Answer... Try again!";
+  showLink(link);
 }
 
 function showLink(link) {
-	document.getElementById('quiz_continue').innerHTML = '<a href="'+link+'"><input type="button" class="btn btn-primary" value="Next Location"></a>';
+  document.getElementById("quiz_continue").innerHTML =
+    '<a href="' +
+    link +
+    '"><input type="button" class="btn btn-primary" value="Next Location"></a>';
 }
-
