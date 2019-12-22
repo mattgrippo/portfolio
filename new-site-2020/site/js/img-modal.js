a2d7265190f931d44b64948ca146361b4afcd64b
Matt Grippo <matt@janz.io>
2019-12-21 22:58:20 -0500

    more changes

--- new-site-2020/site/js/img-modal.js
+++ new-site-2020/site/js/img-modal.js
@@ -162,3 +162,31 @@ modal7.onclick = function() {
 // function partyMode() {
 //   particlestoggle.style.display = "block";
 // }
+
+//new modal
+
+// var modal = document.getElementById("myModal");
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById("myImg");
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.onclick = function() {
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// };
+
+// modal.onclick = function() {
+//   if ($("#myModal").css("display") == "block") {
+//     modal.style.display = "none";
+//   }
+// };
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close-modal")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// };
